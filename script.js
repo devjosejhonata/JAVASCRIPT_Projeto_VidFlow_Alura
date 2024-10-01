f0aeeca2a327dcaae31e27b1d7e99a919a36136e
Jose Jhonata <contatojosejhonata@gmail.com>
2024-09-30 21:25:17 -0300

    Implementando a lógica de pesquisa

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ async function buscarEMostrarVideos(){
 buscarEMostrarVideos();
 
 //barra de pesquisa
-const barraDePesquisa = document.querySelectorAll(".pesquisar__input");
+const barraDePesquisa = document.querySelector(".pesquisar__input");
 
 barraDePesquisa.addEventListener("input", filtrarPesquisa);
 
@@ -36,8 +36,17 @@ function filtrarPesquisa() {
    const videos = document.querySelectorAll(".videos__item");
 
    if (barraDePesquisa.value != "") {
+      for (let video of videos) {
+         let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+         let valorFiltro = barraDePesquisa.value.toLowerCase();
 
+         if(!titulo.includes(valorFiltro)) {
+            video.style.display = "none";
+         } else {
+            video.style.display = "block";
+         }
+      }
    } else {
-      
+      video.style.display = "block";
    }
 }
